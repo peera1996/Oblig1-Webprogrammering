c646977659b905cd79cd8c53ffa049a0202d4707
Peer Magnus Porsborg Kjosmoen <s315274@stud.hioa.no>
2020-02-21 15:37:12 +0100

    Endret filstruktur for å gjøre det mer lesbart i html- og js-fil

--- Oblig1-Webprogrammering/src/main/resources/static/JavaScript.js
+++ Oblig1-Webprogrammering/src/main/resources/static/JavaScript.js
@@ -2,56 +2,56 @@
 var billetter = [];
 var feilmeldinger = [];
 
-function kjoopBillett() {
+    function kjoopBillett() {
 
-    // Legger det brukeren legger inn i inputfeltene i en variabel
-    var film = document.getElementById('film').value;
-    var antall = document.getElementById("antall").value;
-    var fornavn = document.getElementById("fornavn").value;
-    var etternavn = document.getElementById("etternavn").value;
-    var nummer = document.getElementById("nummer").value;
-    var epost = document.getElementById("epost").value;
+        // Legger det brukeren legger inn i inputfeltene i hver sin variabel
+        var film = document.getElementById('film').value;
+        var antall = document.getElementById("antall").value;
+        var fornavn = document.getElementById("fornavn").value;
+        var etternavn = document.getElementById("etternavn").value;
+        var nummer = document.getElementById("nummer").value;
+        var epost = document.getElementById("epost").value;
 
 
-    // Legger inn feilmeldinger til det respektive feltet hvis
-    var tomt = document.getElementsByClassName('verdi');
-    var lengde = tomt.length;
-    for (var i = 0; i < lengde; i++) {
-        if (tomt[i].value == '') {
-            var j = i+1;
-            document.getElementById('feil-' + j).innerHTML = "Må skrive noe inn i " + tomt[i].id;
-            var tomInput = new Boolean(true);
-            feilmeldinger.push(tomt[i]);
-        }
-        // Denne gjør sånn at hvis du fyller ut feltet som var tomt, så forsvinner feilmeldingen
-        else {
-            var j = i + 1;
-            document.getElementById('feil-' + j).innerHTML = ""
-        }
+        // Legger inn feilmeldinger til det respektive feltet hvis
+        var tomt = document.getElementsByClassName('verdi');
+        var lengde = tomt.length;
+        for (var i = 0; i < lengde; i++) {
+            if (tomt[i].value == '') {
+                var j = i+1;
+                document.getElementById('feil-' + j).innerHTML = "Må skrive noe inn i " + tomt[i].id;
+                var tomInput = new Boolean(true);
+                feilmeldinger.push(tomt[i]);
+            }
+            // Denne gjør sånn at hvis du fyller ut feltet som var tomt, så forsvinner feilmeldingen
+            else {
+                var j = i + 1;
+                document.getElementById('feil-' + j).innerHTML = ""
+            }
 
-    };
+        };
 
-    // Denne koden kjører hvis alle inputfeltene ikke er tomme
-    if (!tomInput) {
-        var billett = {
-            film: film,
-            antall: antall,
-            fornavn: fornavn,
-            etternavn: etternavn,
-            nummer: nummer,
-            epost: epost,
-        }
-        billetter.push(billett);
+        // Denne koden kjører hvis alle inputfeltene ikke er tomme
+        if (!tomInput) {
+            var billett = {
+                film: film,
+                antall: antall,
+                fornavn: fornavn,
+                etternavn: etternavn,
+                nummer: nummer,
+                epost: epost,
+            };
+            billetter.push(billett);
 
         // lager tabellen og Legger input-verdier inn i en tabell
         let ut = "<table><tr>" +
             "<th>Film</th><th>Antall</th><th>Fornavn</th><th>Etternavn</th><th>telefonnr</th><th>epost</th>" +
             "</tr>";
-        for (let b of billetter) {
-            ut += "<tr>";
-            ut += "<td>" + b.film + "</td><td>" + b.antall + "</td><td>" + b.fornavn + "</td><td>" + b.etternavn + "</td><td>" + b.nummer + "</td><td>" + b.epost + "</td>";
-            ut += "</tr>";
-        }
+            for (let b of billetter) {
+                ut += "<tr>";
+                ut += "<td>" + b.film + "</td><td>" + b.antall + "</td><td>" + b.fornavn + "</td><td>" + b.etternavn + "</td><td>" + b.nummer + "</td><td>" + b.epost + "</td>";
+                ut += "</tr>";
+            }
 
 
         // Skriver ut billettene til nettsiden
@@ -68,8 +68,8 @@ function kjoopBillett() {
 
 }
 
-//Funksjon som sletter arrayet med splice og setter tabellen som lister arrayet til ""
-function slettBilletter() {
-    billetter.splice(0, billetter.length);
-    document.getElementById("billetter").innerHTML="";
-}
+    //Funksjon som sletter arrayet med splice og setter tabellen som lister arrayet til ""
+    function slettBilletter() {
+        billetter.splice(0, billetter.length);
+        document.getElementById("billetter").innerHTML="";
+    }
